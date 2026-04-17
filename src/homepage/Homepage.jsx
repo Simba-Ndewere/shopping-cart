7166cefb1bbcf7474d4968ca007ab99f04d4ff8c
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-04-17 23:43:53 +0100

    create folder for homepage jsx and css files

---
+++ src/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import homepageCss from './Homepage.module.css';
+
+const Homepage = () => {
+
+    return (
+        <>
+            <div className={homepageCss.container}>
+                <div className={homepageCss.imageHeader}></div>
+            </div>
+        </>
+    )
+}
+
+export default Homepage;
