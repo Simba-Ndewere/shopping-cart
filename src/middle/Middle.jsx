ebba099ccd23db41d648f7974fab8e0a6e73aa93
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-04-27 22:44:53 +0100

    add button to middle component

--- src/middle/Middle.jsx
+++ src/middle/Middle.jsx
@@ -13,18 +13,21 @@ const Middle = () => {
                         <div className={middleCss.icon}>
                             <img className={middleCss.iconImage1} src={toy1} />
                             <div className={middleCss.iconTitle1}>Choose a Toy</div>
-                            <div className={middleCss.iconDescr1}>Pick from over 1000 of the latest must haves.</div>
+                            <div className={middleCss.iconDescr1}>Pick from over 1000  <br />of the latest must haves.</div>
                         </div>
                         <div className={middleCss.icon}>
                             <img className={middleCss.iconImage1} src={toy2} />
                             <div className={middleCss.iconTitle1}>Make Play & Learn</div>
-                            <div className={middleCss.iconDescr1}>Kid led discovery that sparks magination.</div>
+                            <div className={middleCss.iconDescr1}>Kid led discovery that <br /> sparks magination.</div>
                         </div>
                         <div className={middleCss.icon}>
                             <img className={middleCss.iconImage1} src={toy3} />
                             <div className={middleCss.iconTitle1}>Swap & Repeat</div>
-                            <div className={middleCss.iconDescr1}>Return any unloved toy and swap it with something else.</div>
+                            <div className={middleCss.iconDescr1}>Return any unloved toy <br /> & swap it.</div>
                         </div>
+                    </div>
+                    <div className={middleCss.btn}>
+                        <p className={middleCss.headerButton}>Get Started</p>
                     </div>
                 </div>
             </div>
