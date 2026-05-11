6f0fdb33f859bbb96d9f2a18f56101fd507753a3
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-11 22:11:12 +0100

    add links to footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -11,6 +11,26 @@ const Footer = () => {
                     <div className={footerCss.email}>Enter your email</div>
                     <div className={footerCss.subscribeBtn}>Subscribe</div>
                 </div>
+                <div className={footerCss.headers}>
+                    <div className={footerCss.header}>
+                        <p className={footerCss.headerTitle}>Browse Links</p>
+                        <p>Browse Toys</p>
+                        <p>Pricing</p>
+                        <p>Gift</p>
+                    </div>
+                    <div className={footerCss.header}>
+                        <p className={footerCss.headerTitle}>Ground Rules</p>
+                        <p>Terms & Conditions</p>
+                        <p>Privacy Policy</p>
+                        <p>Shipping & Delivery</p>
+                    </div>
+                    <div className={footerCss.header}>
+                        <p className={footerCss.headerTitle}>About Us</p>
+                        <p>How It Works</p>
+                        <p>Reviews</p>
+                        <p>Blog</p>
+                    </div>
+                </div>
             </div>
         </div>
     </>)
