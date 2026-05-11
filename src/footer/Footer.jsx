450ed9b70e18be70e7d8d606e89f0fd631b843dc
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-11 15:14:38 +0100

    add containers to footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,7 +1,13 @@
 import footerCss from './Footer.module.css'
 
 const footerCss = () => {
-
+    return (<>
+        <div className={footerCss.container}>
+            <div className={footerCss.footer}>
+                <div className={footerCss.tittle}></div>
+            </div>
+        </div>
+    </>)
 }
 
 export default footerCss;
