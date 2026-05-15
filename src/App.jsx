dcc4731054472bebb977b7806709cd8d91add1aa
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-15 23:25:36 +0100

    add Naviagtion component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Homepage from './homepage/Homepage.jsx';
 import Middle from './middle/Middle.jsx';
 import Biggertoys from './biggerToys/Biggertoys.jsx';
 import Footer from './footer/Footer.jsx'
+import Navigation from './navigation/Navigation.jsx'
 
 function App() {
 
   return (
     <>
+        <Navigation/>
         <Homepage/>
         <Middle/>
         <Biggertoys/>
