66152b76f69805359c639d80a6feded5f07ee0d4
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-18 11:02:53 +0100

    add close icon to navigation menu

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,16 +1,19 @@
 import navigationCss from './Navigation.module.css';
 import menuImg from '../images/menu.png';
+import closeImg from '../images/close.png';
 import { useState } from 'react';
 
 const Navigation = () => {
 
-    const[menuClicked, setMenuClicked] = useState(false)
+    const [menuClicked, setMenuClicked] = useState(false);
 
+    console.log(menuClicked);
     return <>
         <div className={navigationCss.container}>
-            <img className={navigationCss.menu} src={menuImg} />
-            <div className={navigationCss.sidenav}>
+            <img className={navigationCss.menu} onClick={() => { setMenuClicked(!menuClicked) }} src={menuImg} />
+            <div className={navigationCss.sidenav} style={{width : menuClicked ? 250 : 0}}>
                 <a className={navigationCss.closebtn}>&times;</a>
+                <img className={navigationCss.closeMenu} onClick={() => { setMenuClicked(!menuClicked) }} src={closeImg} />
                 <a>About</a>
                 <a>Services</a>
                 <a>Clients</a>
@@ -18,6 +21,6 @@ const Navigation = () => {
             </div>
         </div>
     </>
-} 
+}
 
 export default Navigation;
