c08d1267b9c5d7f6ea58cc89e08b6fd9e46ec19d
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-18 22:44:50 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Homepage from './homepage/Homepage.jsx';
-import Middle from './middle/Middle.jsx';
-import Biggertoys from './biggerToys/Biggertoys.jsx';
-import Footer from './footer/Footer.jsx'
+import Homepage from './home/homepage/Homepage.jsx';
+import Middle from './home/middle/Middle.jsx';
+import Biggertoys from './home/biggerToys/Biggertoys.jsx';
+import Footer from './home/footer/Footer.jsx'
 import Navigation from './navigation/Navigation.jsx'
 
 function App() {
