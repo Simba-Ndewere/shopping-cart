f8fd6e04df7bea8075ee5317c8c08772636d9b72
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-21 00:00:35 +0100

    remove ternary operator on sidenav and use and operator

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,18 +1,16 @@
 import navigationCss from './Navigation.module.css';
 import menuImg from '../images/menu.png';
-import closeImg from '../images/close.png';
 import { useState } from 'react';
 
 const Navigation = () => {
 
     const [menuClicked, setMenuClicked] = useState(false);
-
-    console.log(menuClicked);
+    
     return <>
         <div className={navigationCss.container}>
             <img className={navigationCss.menu} onClick={() => { setMenuClicked(!menuClicked) }} src={menuImg} />
-            <div className={navigationCss.sidenav} style={{width : menuClicked ? 250 : 0}}>
-                <a className={navigationCss.closebtn} onClick={() => { setMenuClicked(!menuClicked) }} src={closeImg} >&times;</a>
+            <div className={navigationCss.sidenav} style={{width : menuClicked && 250 }}>
+                <a className={navigationCss.closebtn} onClick={() => { setMenuClicked(!menuClicked) }}>&times;</a>
                 <a>About</a>
                 <a>Services</a>
                 <a>Clients</a>
