6df58d5bb9acd05cc464dd529924a6bd2ff45518
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-05-25 23:27:50 +0100

    use react router link over html link

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,18 +1,19 @@
 import navigationCss from './Navigation.module.css';
 import menuImg from '../images/menu.png';
+import { Link } from "react-router";
 import { useState } from 'react';
 
 const Navigation = () => {
 
     const [menuClicked, setMenuClicked] = useState(false);
-    
+
     return <>
         <div className={navigationCss.container}>
             <img className={navigationCss.menu} onClick={() => { setMenuClicked(!menuClicked) }} src={menuImg} />
-            <div className={navigationCss.sidenav} style={{width : menuClicked && 250 }}>
+            <div className={navigationCss.sidenav} style={{ width: menuClicked && 250 }}>
                 <a className={navigationCss.closebtn} onClick={() => { setMenuClicked(!menuClicked) }}>&times;</a>
                 <a>Home</a>
-                <a>Shop</a>
+                <Link to ='shop'>Shop</Link>
                 <a>Cart</a>
             </div>
         </div>
