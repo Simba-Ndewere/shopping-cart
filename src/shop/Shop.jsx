8c80a46924cc7f046fdc34979073f289acfabda7
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-08 22:54:37 +0100

    add grid to shop component for tys on sale

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,8 +1,22 @@
+import shopCss from './Shop.module.css'
 
 const Shop = () => {
     return <>
-        <div>
-            We are in Shop 
+        <div className={shopCss.container}>
+            <div className={shopCss.shop}>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+                <div className={shopCss.image1}></div>
+            </div>
         </div>
     </>
 }
