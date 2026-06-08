0fb0737270487eb2f9624c6ce44a816356382326
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-08 22:57:57 +0100

    add main title to toys being sold

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -3,6 +3,7 @@ import shopCss from './Shop.module.css'
 const Shop = () => {
     return <>
         <div className={shopCss.container}>
+            <div className={shopCss.toys}>Toys</div>
             <div className={shopCss.shop}>
                 <div className={shopCss.image1}></div>
                 <div className={shopCss.image1}></div>
