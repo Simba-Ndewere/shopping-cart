55962540fee8a5fa7ab7802e1935e92c72ff0635
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-08 15:35:54 +0100

    insert child components in home component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,5 +1,15 @@
-const Home = () => {
+import Biggertoys from "./biggerToys/Biggertoys";
+import Homepage from "./homepage/Homepage";
+import Middle from "./middle/Middle";
 
+const Home = () => {
+    return (
+        <>
+            <Homepage/>
+            <Middle/>
+            <Biggertoys/>
+        </>
+    ) 
 }
 
 export default Home;
