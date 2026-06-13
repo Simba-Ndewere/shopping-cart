b0be1f34276a1e779a15e973b91802917c0e4412
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-13 21:24:02 +0100

    create cart product card

--- src/cart/Cart.jsx
+++ src/cart/Cart.jsx
@@ -1,7 +1,28 @@
-import cartCss from './Cart.module.css'
+import cartCss from './Cart.module.css';
+import shop1 from '../images/shop1.jpg';
+import deleteIcon from '../images/delete.png';
 
 const Cart = () => {
-
+    return <>
+        <div className={cartCss.container}>
+            <div className={cartCss.title}>Cart</div>
+            <div className={cartCss.cartCard}>
+                <img src={shop1} className={cartCss.cartImage} />
+                <div className={cartCss.cartInfo}>
+                    <div className={cartCss.name}>Batmobile</div>
+                    <div className={cartCss.price}>£89</div>
+                    <div className={cartCss.quantites}>
+                        <div className={cartCss.quantity}>
+                            <div className={cartCss.decrement}> - </div>
+                            <div className={cartCss.toyQuantity}>1</div>
+                            <div className={cartCss.increment}> + </div>
+                        </div>
+                        <img src={deleteIcon} className={cartCss.deleteIcon} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default Cart;
