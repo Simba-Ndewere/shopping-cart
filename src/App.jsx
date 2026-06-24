4b9ea05762dff8da7e28b76471fafdb7a2fe0eb8
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-24 22:02:46 +0100

    pass props to child elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Navigation from './navigation/Navigation.jsx';
 import { Outlet } from 'react-router';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [cart, setCart] = useState([]);
 
   return (
     <>
       <div className="container">
-        <Navigation />
-        <Outlet />
+        <Navigation cart={cart}/>
+        <Outlet context={[cart, setCart]}/>
       </div>
     </>
   )
