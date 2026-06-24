efddfa79e5ba47693b2fa49a68497e5ff60489af
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-24 22:09:38 +0100

    add selected products to cart state

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -12,13 +12,15 @@ import shop9 from '../images/shop9.jpg';
 import shop10 from '../images/shop10.jpg';
 import shop11 from '../images/shop11.jpg';
 import shop12 from '../images/shop12.jpg';
-import { useEffect, useState } from 'react';
 
+import { useEffect, useState } from 'react';
+import { useOutletContext } from "react-router";
 
 const Shop = () => {
 
     const toysArray = [shop1, shop2, shop3, shop4, shop5, shop6, shop7, shop8, shop9, shop10, shop11, shop12];
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useOutletContext();
 
     useEffect(() => {
         fetch('https://dummyjson.com/products/category/smartphones/?delay=2000')
@@ -26,31 +28,57 @@ const Shop = () => {
             .then((res) => setProducts(res.products));
     }, []);
 
-    //console.log(products[0].brand);
-    console.log(products)
+    console.log('increment');
+    console.log(cart);
+
+
+    const increment = (productId, price) => {
+        if (!cart.find( ({id}) => id === productId)) {
+            setCart([
+                ...cart,
+                {
+                    id: productId,
+                    quantity: 1,
+                    price: price
+                }
+            ])
+        } else {
+            setCart(cart.map(product => {
+                if (productId == product.id) {
+                    const quantityVal = product.quantity;
+                    return { ...product, quantity: quantityVal + 1 }
+                } else {
+                    return product;
+                }
+            }))
+        }
+    }
+
+    const decrement = (productId, price) => {
+
+    }
 
     return <div className={shopCss.contBackground}>
+
         <div className={shopCss.container}>
             <div className={shopCss.toys}>Store</div>
             <div className={shopCss.shop}>
 
-
                 {products.length != 0 ? toysArray.map((toy, index) => {
-                        return <div key={products[index].id} className={shopCss.toyCont}>
-                            <img className={shopCss.image1} src={toy} />
-                            <div className={shopCss.title}>{products[index].title}</div>
-                            <div className={shopCss.description}>{products[index].description}</div>
-                            <div className={shopCss.priceQuantity}>
-                                <div className={shopCss.price}>{products[index].price}</div>
-                                <div className={shopCss.quantity}>
-                                    <div className={shopCss.decrement}> - </div>
-                                    <div className={shopCss.toyQuantity}>1</div>
-                                    <div className={shopCss.increment}> + </div>
-                                </div>
+                    return <div key={products[index].id} className={shopCss.toyCont}>
+                        <img className={shopCss.image1} src={toy} />
+                        <div className={shopCss.title}>{products[index].title}</div>
+                        <div className={shopCss.description}>{products[index].description}</div>
+                        <div className={shopCss.priceQuantity}>
+                            <div className={shopCss.price}>{products[index].price}</div>
+                            <div className={shopCss.quantity}>
+                                <div className={shopCss.decrement} onClick={() => decrement(products[index].id, products[index].price)}> - </div>
+                                <div className={shopCss.toyQuantity}>1</div>
+                                <div className={shopCss.increment} onClick={() => increment(products[index].id, products[index].price)}> + </div>
                             </div>
                         </div>
-                    }) : <div>Loading..</div>}
-
+                    </div>
+                }) : <div>Loading..</div>}
             </div>
         </div>
     </div>
