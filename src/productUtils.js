18d1038a36b1ecf46b6ead575995f52f871789bd
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-25 23:40:15 +0100

    create a utility class with helper functions for reuse

---
+++ src/productUtils.js
@@ -0,0 +1,38 @@
+export function increment(cart, setCart, productId, price) {
+    if (!cart.find(({ id }) => id === productId)) {
+        setCart([
+            ...cart,
+            {
+                id: productId,
+                quantity: 1,
+                price: price
+            }
+        ])
+    } else {
+        setCart(cart.map(product => {
+            if (productId == product.id) {
+                const quantityVal = product.quantity;
+                return { ...product, quantity: quantityVal + 1 }
+            } else {
+                return product;
+            }
+        }))
+    }
+}
+
+export function decrement(cart, setCart, productId) {
+    const product = cart.find(({ id }) => id === productId);
+    if (product) {
+        product.quantity > 1 ?
+            setCart(cart.map(product => {
+                if (productId == product.id) {
+                    let quantityVal = product.quantity;
+                    return { ...product, quantity: quantityVal - 1 }
+                } else {
+                    return product;
+                }
+            })) : setCart(
+                cart.filter(a => a.id !== productId)
+            );
+    }
+}
