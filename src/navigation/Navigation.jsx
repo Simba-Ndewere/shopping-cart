0f58f041590f364ca3eccc5331f9739666828b8d
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-25 22:33:43 +0100

    update cart size in navugation real time

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -3,21 +3,30 @@ import menuImg from '../images/menu.png';
 import { Link } from "react-router";
 import { useState } from 'react';
 
-const Navigation = ({cart}) => {
+const Navigation = ({ cart }) => {
 
     const [menuClicked, setMenuClicked] = useState(false);
 
     console.log("nav");
     console.log(cart);
 
+    let cartSize = 0;
+
+    for(let a = 0; a < cart.length; a++){
+        cartSize = cartSize + cart[a].quantity;
+    }
+
     return <div className={navigationCss.contBackground}>
         <div className={navigationCss.container}>
             <img className={navigationCss.menu} onClick={() => { setMenuClicked(!menuClicked) }} src={menuImg} />
             <div className={navigationCss.sidenav} style={{ width: menuClicked && 250 }}>
                 <a className={navigationCss.closebtn} onClick={() => { setMenuClicked(!menuClicked) }}>&times;</a>
-                <Link to ='home'>Home</Link>
-                <Link to ='shop'>Store</Link>
-                <Link to ='cart'>Cart</Link>
+                <Link to='home'>Home</Link>
+                <Link to='shop'>Store</Link>
+                <div className={navigationCss.cartCont}>
+                    <Link to='cart'>Cart</Link>
+                    <a>{cartSize}</a>
+                </div>
             </div>
         </div>
     </div>
