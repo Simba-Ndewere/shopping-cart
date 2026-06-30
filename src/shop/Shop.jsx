10951bfb8a062e01e827c04ef757317404c67744
Simba Ndewere <148736984+Simba-Ndewere@users.noreply.github.com>
2026-06-30 21:33:41 +0100

    use products state instead of empty array for iteration

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -6,7 +6,6 @@ import { increment, decrement } from '../productUtils';
 
 const Shop = () => {
 
-    const productArray = ["", "", "", "", "", "", "", "", "", "", "", ""];
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useOutletContext();
 
@@ -22,21 +21,21 @@ const Shop = () => {
             <div className={shopCss.toys}>Store</div>
             <div className={shopCss.shop}>
 
-                {products.length != 0 ? productArray.map((product, index) => {
+                {products.length != 0 ? products.map((product) => {
 
-                    const cartItem = cart.find(({ id }) => id === products[index].id);
+                    const cartItem = cart.find(({ id }) => id === product.id);
 
-                    return <div key={products[index].id} className={shopCss.toyCont}>
-                        <img className={shopCss.image1} src={products[index].images[1]} />
-                        <div className={shopCss.title}>{products[index].title}</div>
-                        <div className={shopCss.description}>{products[index].description}</div>
+                    return <div key={product.id} className={shopCss.toyCont}>
+                        <img className={shopCss.image1} src={product.images[1]} />
+                        <div className={shopCss.title}>{product.title}</div>
+                        <div className={shopCss.description}>{product.description}</div>
                         <div className={shopCss.priceQuantity}>
-                            <div className={shopCss.price}>{products[index].price}</div>
+                            <div className={shopCss.price}>{product.price}</div>
                             <div className={shopCss.quantity}>
-                                <div className={shopCss.decrement} onClick={() => decrement(cart, setCart, products[index].id)}> - </div>
+                                <div className={shopCss.decrement} onClick={() => decrement(cart, setCart, product.id)}> - </div>
                                 <div className={shopCss.toyQuantity}> {cartItem ? cartItem.quantity : 0}
                                 </div>
-                                <div className={shopCss.increment} onClick={() => increment(cart, setCart, products[index].id, products[index].price, products[index].images[1], products[index].title)}> + </div>
+                                <div className={shopCss.increment} onClick={() => increment(cart, setCart, product.id, product.price, product.images[1], product.title)}> + </div>
                             </div>
                         </div>
                     </div>
